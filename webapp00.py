aacc3104b4d29b3e0d8dd66423f481cd3b862e69
Nicholas-CT <141649278+Nicholas-CT@users.noreply.github.com>
2023-10-02 10:47:17 -0300

    webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,12 @@
 import streamlit as st
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Prof. Massaki Igarashi")
-
+st.title("1º app Nicholas Craveiro Tonhi")
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Teste de Cabeçalho")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Bem vindos!")
+st.subheader("Bem vindo!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
